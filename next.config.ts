025995720f28994a5e6ae7fa233f547b79fafc9e
Le Van Quy <vannn.quy@gmail.com>
2025-05-18 20:14:29 +0700

    up

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   webpack(config: Configuration, { isServer }) {
     config.module?.rules?.push({
       test: /\.svg$/,
